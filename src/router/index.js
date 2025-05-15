327fcba8a012db4fc31ae28395caa4e3d5d145ba
Daniel Valdizán Grijalba <dvaldizangrijalba@gmail.com>
2025-05-15 18:08:34 -0500

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -1,1 +1,28 @@
+import { createRouter, createWebHistory } from 'vue-router';
+import Dashboard from '@/views/Dashboard.vue';
+import RegistroProducto from '@/views/RegistroProducto.vue';
+
+const routes = [
+  {
+    path: '/',
+    redirect: '/dashboard' // Redirige directamente al Dashboard
+  },
+  {
+    path: '/dashboard',
+    name: 'Dashboard',
+    component: Dashboard
+  },
+  {
+    path: '/registrar-producto',
+    name: 'RegistroProducto',
+    component: RegistroProducto
+  }
+];
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes
+});
+
+export default router;
 
