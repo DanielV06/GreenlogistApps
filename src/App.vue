899893c148de1358c02b439201f122373e990fba
Daniel Valdizán Grijalba <dvaldizangrijalba@gmail.com>
2025-05-15 17:58:28 -0500

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <router-view />
+</template>
+
+<script>
+export default {
+  name: 'App',
+};
+</script>
+
+<style>
+body {
+  font-family: Arial, sans-serif;
+  margin: 0;
+  background-color: #f1f8e9;
+}
+</style>
