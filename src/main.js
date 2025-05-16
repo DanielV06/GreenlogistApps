156f9a66625cadffa41a7f689b8150c40047efd1
Roy Fernandez <u20221b778@upc.edu.pe>
2025-05-15 20:07:54 -0500

    feat(routes4): connect routes

--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
-import { createApp } from 'vue';
-import App from './App.vue';
-import router from './router';
-import './assets/styles.css';
+import { createApp } from 'vue'
+import App from './App.vue'
+import router from './router' 
 
-createApp(App).use(router).mount('#app');
+
+const app = createApp(App);
+app.use(router); // usa el router
+app.mount('#app');
