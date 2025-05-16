eeb994c887fe36137136fc7313887500c51932f9
Roy Fernandez <u20221b778@upc.edu.pe>
2025-05-15 20:05:25 -0500

    feat(Routes1): add code in routes

--- src/router/index.js
+++ src/router/index.js
@@ -3,3 +3,26 @@ import LoginView from '../views/LoginView.vue';
 import RegisterView from '../views/RegisterView.vue'; 
 import DashboardView from '../views/DashboardView.vue'; 
 
+const routes = [
+  {
+    path: '/',
+    redirect: '/login',
+  },
+  {
+    path: '/login',
+    name: 'Login',
+    component: LoginView,
+  },
+  {
+    path: '/register',
+    name: 'Register',
+    component: RegisterView,
+  },
+  {
+    path: '/dashboard', 
+    name: 'Dashboard', 
+    component: DashboardView, 
+    meta: { requiresAuth: true },
+  },
+];
+
