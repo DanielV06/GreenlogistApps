f562d7ba011ee22bc0665b4bdf58d4fa3533e367
Roy Fernandez <u20221b778@upc.edu.pe>
2025-05-15 20:04:17 -0500

    feat(Routes): add routes configuration

--- src/router/index.js
+++ src/router/index.js
@@ -1,28 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router';
-import Dashboard from '@/views/Dashboard.vue';
-import RegistroProducto from '@/views/RegistroProducto.vue';
-
-const routes = [
-  {
-    path: '/',
-    redirect: '/dashboard' // Redirige directamente al Dashboard
-  },
-  {
-    path: '/dashboard',
-    name: 'Dashboard',
-    component: Dashboard
-  },
-  {
-    path: '/registrar-producto',
-    name: 'RegistroProducto',
-    component: RegistroProducto
-  }
-];
-
-const router = createRouter({
-  history: createWebHistory(),
-  routes
-});
-
-export default router;
+import LoginView from '../views/LoginView.vue'; 
+import RegisterView from '../views/RegisterView.vue'; 
+import DashboardView from '../views/DashboardView.vue'; 
 
