32779db357ce4eb0291b3539eae55d4a74056c3a
Roy Fernandez <u20221b778@upc.edu.pe>
2025-05-15 20:32:20 -0500

    feat(login): add login

---
+++ src/views/LoginView.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="auth-form-container">
+    <h1>Iniciar Sesión</h1>
+    <form @submit.prevent="handleLogin">
+      <div class="form-group">
+        <label for="email">Correo electrónico</label>
+        <input type="email" id="email" v-model="email" required />
+      </div>
+      <div class="form-group">
+        <label for="password">Contraseña</label>
+        <input type="password" id="password" v-model="password" required />
+      </div>
+      <div v-if="errorMessage" class="error-message">{{ errorMessage }}</div>
+      <button type="submit" class="btn-submit">Ingresar</button>
+    </form>
+    <div class="auth-link">
+      ¿No tienes cuenta? <router-link to="/register">Regístrate aquí</router-link>
+    </div>
+  </div>
+</template>
+
