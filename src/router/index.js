5c71f864ad9352bbc5c0af4ee3c66871fae10415
Roy Fernandez <u20221b778@upc.edu.pe>
2025-05-15 20:06:28 -0500

    feat(routes3): add more code

--- src/router/index.js
+++ src/router/index.js
@@ -30,3 +30,23 @@ const router = createRouter({
   history: createWebHistory(process.env.BASE_URL), 
   routes,
 });
+
+router.beforeEach((to, from, next) => {
+  const loggedIn = localStorage.getItem('currentUser');
+  const requiresAuth = to.matched.some(record => record.meta.requiresAuth);
+
+  if (requiresAuth && !loggedIn) {
+    next('/login');
+  } else if ((to.name === 'Login' || to.name === 'Register') && loggedIn && requiresAuth) {
+    // Si el usuario está logueado e intenta ir a Login o Register,
+    next('/dashboard');
+  } else if ((to.name === 'Login' || to.name === 'Register') && loggedIn) {
+    // Si el usuario ya está logueado e intenta ir a Login o Register, redirige al dashboard
+    next('/dashboard');
+  }
+  else {
+    next();
+  }
+});
+
+export default router;
